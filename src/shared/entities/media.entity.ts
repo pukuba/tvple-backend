1e885cc5d4abff83575e5c57485b7410f7b86163
SeungWon <20sunrin041@sunrint.hs.kr>
2021-10-14 23:39:56 +0900

    feat: Add MediaEntity

---
+++ src/shared/entities/media.entity.ts
@@ -0,0 +1,60 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    BeforeInsert,
+    JoinTable,
+    ManyToMany,
+    OneToMany,
+    PrimaryColumn,
+    Table,
+} from "typeorm"
+import * as shortid from "shortid"
+import * as argon2 from "argon2"
+
+@Entity("media")
+export class MediaEntity {
+    constructor(partial: Partial<MediaEntity>) {
+        Object.assign(this, partial)
+    }
+
+    @PrimaryColumn({
+        type: "varchar",
+        length: 20,
+        primary: true,
+        nullable: false,
+    })
+    mediaId: string
+
+    @Column({
+        type: "text",
+        nullable: false,
+    })
+    url: string
+
+    @Column({
+        type: "varchar",
+        length: 20,
+        nullable: false,
+    })
+    userId: string
+
+    @Column({
+        type: "varchar",
+        length: 2000,
+        nullable: false,
+    })
+    description: string
+
+    @Column({
+        type: "varchar",
+        length: 80,
+        nullable: false,
+    })
+    name: string
+
+    @BeforeInsert()
+    setId() {
+        this.mediaId = shortid.generate()
+    }
+}
